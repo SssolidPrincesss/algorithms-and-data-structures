965e3c691d3cabe940d18be44203f72b07bc7a92
SolidPrincess <glden.chi@gmail.com>
2025-02-18 09:55:01 +0500

    ,

--- Merge Two Sorted Lists.java
+++ Merge Two Sorted Lists.java
@@ -1,13 +1,33 @@
-public class Solution {
-    public int[] twoSum(int[] nums, int target) {
-      
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
+import java.util.ArrayList;
+import java.util.Collections;
+import java.lang.*;
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode golova = new ListNode(0);
+        ListNode hvost = golova;
+
+        while(list1 != null && list2 != null){
+            if (list1.val < list2.val){
+                hvost.next = list1;
+                list1 = list1.next;
             }
+            else{
+                hvost.next = list2;
+                list2 = list2.next; 
+            }
+            hvost = hvost.next;
         }
-        return null; 
+
+        if (list1 != null){
+            hvost.next = list1;
+        }
+
+        if (list2 != null){
+            hvost.next = list2;
+        }
+
+        return golova.next;
     }
 }
