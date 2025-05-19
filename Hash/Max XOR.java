c2e65c6b4e975a3419d5beb099e3f7812ef6116d
SolidPrincess <glden.chi@gmail.com>
2025-05-19 22:06:32 +0500

    add Max XOR

---
+++ Hash/Max XOR.java
@@ -0,0 +1,49 @@
+class Node {
+    HashMap<Integer, Node> children;
+    Node() {
+        this.children = new HashMap<>();
+    }
+}
+
+class Trie {
+    Node root;
+    
+    Trie() {
+        this.root = new Node();
+    }
+    
+    public void insert(int[] A) {
+        for(int num : A) {
+            Node curr = this.root;
+            for(int i=31;i>=0;i--) {
+                int currBit = (num >> i) & 1;
+                if(!curr.children.containsKey(currBit)) 
+                    curr.children.put(currBit, new Node());
+                curr = curr.children.get(currBit);
+            }
+        }
+    }
+}
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        int max = 0, mask = 0;
+        for (int i = 31; i >= 0; i--) {
+            mask |= (1 << i);
+            Set<Integer> prefixSet = new HashSet<>();
+
+            for (int num : nums) {
+                prefixSet.add(num & mask);
+            }
+            
+
+            int candidate = max | (1 << i);
+            for (int prefix : prefixSet) {
+                if (prefixSet.contains(candidate ^ prefix)) {
+                    max = candidate;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
